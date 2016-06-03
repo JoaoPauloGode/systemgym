37d8bf1199aae7da57eaac323ef90cd97fb92c18
Joao Paulo Gode <joopaulogodliberal78@gmail.com>
2016-06-03 12:04:52 -0300

    Melhoria da tela de dados do aluno -labels adicionados -posições dos itens adaptados
    Refs T79

--- src/view/TelaDadosAluno.java
+++ src/view/TelaDadosAluno.java
@@ -15,7 +15,7 @@ public class TelaDadosAluno extends JFrame {
 	 * @author Joao Paulo
 	 */
 	private static final long serialVersionUID = 1L;
-	JLabel telaFundoLabel;
+	JLabel telaFundoLabel, nomeLabel, telefoneLabel, enderecoLabel, cpfLabel, idLabel, saldoDevedorLabel;
 	JTextField nomeField, telefoneField, enderecofield, cpfField, idField, saldoDevedorField;
 	
 	ListaDeAlunos listaDeAlunos;
@@ -34,6 +34,27 @@ public class TelaDadosAluno extends JFrame {
 		
 		handlerPesquisa=new ButtonHandlerPesquisa(telaPesquisaAluno, listaDeAlunos);
 		
+		nomeLabel=new JLabel("Nome: ");
+		telefoneLabel=new JLabel("Telefone: ");
+		enderecoLabel=new JLabel("Endereço: ");
+		cpfLabel=new JLabel("CPF: ");
+		idLabel=new JLabel("ID: ");
+		saldoDevedorLabel=new JLabel("Saldo Devedor: ");
+		
+		telaFundoLabel.add(nomeLabel);
+		telaFundoLabel.add(telefoneLabel);
+		telaFundoLabel.add(enderecoLabel);
+		telaFundoLabel.add(cpfLabel);
+		telaFundoLabel.add(idLabel);
+		telaFundoLabel.add(saldoDevedorLabel);
+		
+		nomeLabel.setBounds(20, 50, 180, 30);
+		telefoneLabel.setBounds(20, 90, 180, 30);
+		enderecoLabel.setBounds(20, 130, 180, 30);
+		cpfLabel.setBounds(20, 170, 180, 30);
+		idLabel.setBounds(20, 210, 180, 30);
+		saldoDevedorLabel.setBounds(20, 250, 180, 30);
+		//=====================================================================================================================		
 		nomeField=new JTextField(listaDeAlunos.listaAlunos.get(handlerPesquisa.getPosId()).getNome());
 		telefoneField=new JTextField(listaDeAlunos.listaAlunos.get(handlerPesquisa.getPosId()).getTelefone());
 		enderecofield=new JTextField(listaDeAlunos.listaAlunos.get(handlerPesquisa.getPosId()).getEndereco());
@@ -48,13 +69,13 @@ public class TelaDadosAluno extends JFrame {
 		telaFundoLabel.add(idField);
 		telaFundoLabel.add(saldoDevedorField);
 		
-		nomeField.setBounds(50, 50, 180, 30);
-		telefoneField.setBounds(50, 90, 180, 30);
-		enderecofield.setBounds(50, 130, 180, 30);
-		cpfField.setBounds(50, 170, 180, 30);
-		idField.setBounds(50, 210, 180, 30);
-		saldoDevedorField.setBounds(50, 250, 180, 30);
-
+		nomeField.setBounds(70, 50, 190, 30);
+		telefoneField.setBounds(80, 90, 180, 30);
+		enderecofield.setBounds(80, 130, 180, 30);
+		cpfField.setBounds(50, 170, 210, 30);
+		idField.setBounds(45, 210, 215, 30);
+		saldoDevedorField.setBounds(110, 250, 150, 30);
+		
 		setSize(500, 500);
 		setLocationRelativeTo(null);
 		setVisible(true);
